6a5c3bf8f3bb4e11e788fe3432cf26192e435dc1
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-07-21 08:29:34 +0530

    comments removed from login.jsx

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,5 +1,3 @@
-// import { useState } from "react";
-// import axiosRequest from "../utils/axiosRequest";
 import {
   Grid,
   Box,
@@ -153,54 +151,3 @@ const Login = () => {
 };
 
 export default Login;
-
-// const [userValue, setUserValue] = useState({
-//   email: "",
-//   password: "",
-//   confirmPassword: "",
-// });
-
-// const handleChange = (event) => {
-//   console.log(event.target.name, event.target.value);
-//   setUserValue({
-//     ...userValue,
-//     [event.target.name]: event.target.value,
-//   });
-// };
-
-// const handleSubmit = async (event) => {
-//   event.preventDefault();
-
-//   if (userValue.password !== userValue.confirmPassword) {
-//     window.alert("password should be matched with confrim password");
-//     return;
-//   }
-//   axiosRequest({ method: "post", url: "/register", data: userValue });
-// };
-
-/* <div className="App">
-<form onSubmit={handleSubmit}>
-  <input
-    name="email"
-    type="email"
-    value={userValue.email}
-    onChange={handleChange}
-    required={true}
-  />
-  <input
-    name="password"
-    type="password"
-    value={userValue.password}
-    onChange={handleChange}
-    required={true}
-  />
-  <input
-    name="confirmPassword"
-    type="password"
-    value={userValue.confirmPassword}
-    onChange={handleChange}
-    required={true}
-  />
-  <button type="submit"> submit </button>
-</form>
-</div> */
