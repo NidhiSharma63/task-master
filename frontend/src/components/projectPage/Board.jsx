7f448ba698b01e92f6e91d832d23f66bc59ad382
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-07-23 08:34:35 +0530

    box code refactored

--- frontend/src/components/projectPage/Board.jsx
+++ frontend/src/components/projectPage/Board.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { Grid, Typography, Box } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import TaskBoxConatiner from "./components/TaskBoxConatiner";
 
 const Board = () => {
   const { active_project } = useParams();
@@ -16,75 +18,10 @@ const Board = () => {
         pl: 3,
       }}
     >
-      <Grid item xs={2.8} sx={{ height: "100%" }}>
-        <Typography sx={{ fontWeight: 600 }} variant="subtitle">
-          {" "}
-          Todo
-        </Typography>
-        <Box
-          sx={{
-            width: "100%",
-            // border: "1px solid green",
-            mt: 1,
-
-            height: "calc(100% - 30px)",
-            borderRadius: ".6rem",
-            boxShadow: "0px 0px 4px 1px #00000014",
-          }}
-          className="box"
-        ></Box>
-      </Grid>
-      <Grid item xs={2.8}>
-        <Typography sx={{ fontWeight: 600 }} variant="subtitle">
-          In progress
-          <Box
-            sx={{
-              width: "100%",
-              // border: "1px solid green",
-              mt: 1,
-
-              height: "calc(100% - 30px)",
-              borderRadius: ".6rem",
-              boxShadow: "0px 0px 4px 1px #00000014",
-            }}
-            className="box"
-          ></Box>
-        </Typography>
-      </Grid>
-      <Grid item xs={2.8}>
-        <Typography sx={{ fontWeight: 600 }} variant="subtitle">
-          In priority
-        </Typography>
-        <Box
-          sx={{
-            width: "100%",
-            // border: "1px solid green",
-            mt: 1,
-
-            height: "calc(100% - 30px)",
-            borderRadius: ".6rem",
-            boxShadow: "0px 0px 4px 1px #00000014",
-          }}
-          className="box"
-        ></Box>
-      </Grid>
-      <Grid item xs={2.8}>
-        <Typography sx={{ fontWeight: 600 }} variant="subtitle">
-          Done
-        </Typography>
-        <Box
-          sx={{
-            width: "100%",
-            // border: "1px solid green",
-            mt: 1,
-
-            height: "calc(100% - 30px)",
-            borderRadius: ".6rem",
-            boxShadow: "0px 0px 4px 1px #00000014",
-          }}
-          className="box"
-        ></Box>
-      </Grid>
+      <TaskBoxConatiner name={"Todo"} />
+      <TaskBoxConatiner name={"In progress"} />
+      <TaskBoxConatiner name={"In priority"} />
+      <TaskBoxConatiner name={"Done"} />
     </Grid>
   );
 };
