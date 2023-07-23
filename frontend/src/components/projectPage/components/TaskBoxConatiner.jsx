3c948d81a9553c8138250f99bedf2fad2dd380fe
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-07-23 09:00:42 +0530

    auto resize added

--- frontend/src/components/projectPage/components/TaskBoxConatiner.jsx
+++ frontend/src/components/projectPage/components/TaskBoxConatiner.jsx
@@ -1,7 +1,15 @@
 import { Box, Grid, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import { useRef } from "react";
 
 const TaskBoxConatiner = ({ name }) => {
+  const textAreaRef = useRef();
+
+  function autoResize() {
+    textAreaRef.current.style.height = "auto";
+    textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`; // Set the height to the scroll height
+  }
+
   return (
     <Grid item xs={2.8} sx={{ height: "100%" }}>
       <Box
@@ -30,9 +38,21 @@ const TaskBoxConatiner = ({ name }) => {
           height: "calc(100% - 30px)",
           borderRadius: ".6rem",
           boxShadow: "0px 0px 4px 1px #00000014",
+          display: "flex",
+          alignItems: "flex-start",
+          justifyContent: "center",
         }}
         className="box"
-      ></Box>
+      >
+        <textarea
+          name=""
+          id=""
+          onInput={autoResize}
+          cols="34"
+          ref={textAreaRef}
+          className="textarea-for-adding-task"
+        ></textarea>
+      </Box>
     </Grid>
   );
 };
