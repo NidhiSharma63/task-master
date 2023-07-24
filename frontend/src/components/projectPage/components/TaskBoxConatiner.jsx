665200640e3bd45a307d6ef760559c451565178e
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-07-24 16:29:17 +0530

    texareas now added in another working way

--- frontend/src/components/projectPage/components/TaskBoxConatiner.jsx
+++ frontend/src/components/projectPage/components/TaskBoxConatiner.jsx
@@ -2,50 +2,36 @@ import { Box, Grid, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { useEffect, useRef, useState } from "react";
 import ReactDOMServer from "react-dom/server";
+import DoneIcon from "@mui/icons-material/Done";
+import ClearIcon from "@mui/icons-material/Clear";
+const TaskBoxContainer = ({ name }) => {
+  const [textAreaValues, setTextAreaValues] = useState([]);
 
-const TaskBoxConatiner = ({ name }) => {
-  const textAreaRef = useRef();
-
-  const refMap = {
-    Todo: useRef(null),
-    "In progress": useRef(null),
-    "In priority": useRef(null),
-    Done: useRef(null),
+  const handleAddTask = () => {
+    setTextAreaValues((prevValues) => [...prevValues, ""]);
   };
 
-  function autoResize() {
-    textAreaRef.current.style.height = "auto";
-    textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`; // Set the height to the scroll height
-  }
-
-  const handleChange = () => {
-    console.log("first");
+  const handleChange = (event, index, newValue) => {
+    // console.log(event.target.dataset);
+    setTextAreaValues((prevValues) => {
+      const copyValues = [...prevValues];
+      copyValues[index] = newValue;
+      return copyValues;
+    });
   };
 
-  const handleClick = (name) => {
-    if (refMap[name].current) {
-      const iconString = ReactDOMServer.renderToString(<AddIcon />);
-      refMap[name].current.insertAdjacentHTML(
-        "afterbegin",
-        `<div>
-        <textarea
-          id="${name}"
-          onChange = {handleChange}
-          cols="34"
-          class="textarea-for-adding-task"
-        ></textarea>
-        ${iconString}
-      </div>`
-      );
-      const textArea = document.getElementById(name);
-      textArea.addEventListener("input", () => {
-        textArea.style.height = "auto";
-        textArea.style.height = `${textArea.scrollHeight}px`;
-      });
-    }
+  const handleBlur = () => {
+    console.log(textAreaValues);
   };
 
-  //  onInput={${autoResize}} // ref={${textAreaRef}}
+  /**
+   * for managing style of textarea
+   */
+  const handleInput = (event) => {
+    event.target.style.height = "auto";
+    event.target.style.height = `${event.target.scrollHeight}px`;
+  };
+
   return (
     <Grid item xs={2.8} sx={{ height: "100%" }}>
       <Box
@@ -56,12 +42,11 @@ const TaskBoxConatiner = ({ name }) => {
           justifyContent: "space-between",
         }}
       >
-        <Typography sx={{ fontWeight: 600 }} variant="subtitle">
-          {" "}
+        <Typography sx={{ fontWeight: 600 }} variant="subtitle1">
           {name}
         </Typography>
         <AddIcon
-          onClick={() => handleClick(name)}
+          onClick={handleAddTask}
           sx={{
             cursor: "pointer",
             color: (theme) => theme.palette.primary.main,
@@ -76,15 +61,26 @@ const TaskBoxConatiner = ({ name }) => {
           borderRadius: ".6rem",
           boxShadow: "0px 0px 4px 1px #00000014",
           display: "flex",
-          alignItems: "center",
+          alignItems: "flex-start",
           justifyContent: "flex-start",
           flexDirection: "column",
+          overflowY: "auto",
+          p: 1,
         }}
-        className="box"
-        ref={refMap[name]}
-      ></Box>
+      >
+        {textAreaValues.map((value, index) => (
+          <textarea
+            key={index}
+            value={value}
+            data-id={name}
+            onChange={(event) => handleChange(event, index, event.target.value)}
+            onBlur={handleBlur}
+            onInput={handleInput}
+            className="textArea"
+          />
+        ))}
+      </Box>
     </Grid>
   );
 };
-
-export default TaskBoxConatiner;
+export default TaskBoxContainer;
