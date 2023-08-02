7288142afb14b67725e82ac8d6a4edbc004d6146
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-08-02 12:18:23 +0530

    task updation made accoring to status

--- backend/controllers/api/apiUpdateRoutes.js
+++ backend/controllers/api/apiUpdateRoutes.js
@@ -3,21 +3,57 @@ const Task = require("../../models/taskSchema");
 const updateTaskApi = async (req, res) => {
   try {
     const taskBody = req.body;
-    const { _id, userId } = taskBody;
-    const taskObj = await Task.findOne({ _id, userId });
-
-    if (!taskObj) {
-      throw new Error("Task not found!");
+    const {
+      _id,
+      userId,
+      index: previousIndex,
+      currentIndex,
+      status,
+      projectName,
+    } = taskBody;
+    // const taskObj = await Task.findOne({ _id, userId });
+    if (currentIndex === undefined || previousIndex === undefined) {
+      throw new Error("Something is missing currentIndex or previousIndex");
     }
-    taskObj.task = taskBody.task;
-    taskObj.status = taskBody.status;
-    taskObj.dueDate = taskBody.dueDate;
-    taskObj.description = taskBody.description;
 
+    if (currentIndex > previousIndex) {
+      const tasksInRange = await Task.find({
+        userId,
+        status: status,
+        projectName,
+        index: { $gt: previousIndex, $lte: currentIndex },
+      });
+
+      // Update the index of the found tasks and save them
+      const updateTasksPromises = tasksInRange?.map(async (item) => {
+        item.index = item.index - 1;
+        await item.save();
+      });
+
+      await Promise.all(updateTasksPromises);
+    }
+    if (currentIndex < previousIndex) {
+      const tasksInRange = await Task.find({
+        userId,
+        status: status,
+        projectName,
+        index: { $gte: currentIndex, $lt: previousIndex },
+      });
+      // Update the index of the found tasks and save them
+      const updateTasksPromises = tasksInRange?.map(async (item) => {
+        item.index = item.index + 1;
+        await item.save();
+      });
+
+      await Promise.all(updateTasksPromises);
+    }
+    // now update the original task as well
+    const taskObj = await Task.findOne({ _id, userId });
+    taskObj.index = currentIndex;
     await taskObj.save();
     res.json({ data: taskObj });
   } catch (error) {
-    res.status(500).json({ msg: error.message });
+    res.status(400).json({ msg: error.message });
   }
 };
 
