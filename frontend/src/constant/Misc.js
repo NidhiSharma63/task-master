36dd43de25514a72438f9ffc95c2077f653ad155
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-08-05 19:16:15 +0530

    base url changed

--- frontend/src/constant/Misc.js
+++ frontend/src/constant/Misc.js
@@ -1,5 +1,4 @@
-// export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const BASE_URL = "http://localhost:3000/api/v1";
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const KEY_FOR_STORING_TOKEN = "token_for_todo";
 export const KEY_FOR_STORING_USER_DETAILS = "user_details_for_todo";
 export const KEY_FOR_STORING_ACTIVE_PROJECT = "active_project_for_todo";
