f217209bbbb208cce6ccfada9599b08b82567dff
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-08-07 09:47:41 +0530

    password validation added

--- frontend/src/constant/regex.js
+++ frontend/src/constant/regex.js
@@ -1,1 +1,2 @@
 export const regexToTargetAllBlankSpaces = /^\s*\S[^]*$/;
+export const passwordRegex = /^(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
--- frontend/src/constant/validation.js
+++ frontend/src/constant/validation.js
@@ -1,5 +1,5 @@
 import { ref, object, string } from "yup";
-import { regexToTargetAllBlankSpaces } from "../constant/regex";
+import { regexToTargetAllBlankSpaces, passwordRegex } from "../constant/regex";
 
 export const loginSchema = object().shape({
   email: string().required("Enter your email").email("Invalid email format"),
@@ -12,7 +12,11 @@ export const registerSchema = object().shape({
   email: string().required("Enter your email").email("Invalid email format"),
   password: string()
     .required("Password required")
-    .matches(regexToTargetAllBlankSpaces, "This field cannot contain spaces"),
+    .matches(regexToTargetAllBlankSpaces, "This field cannot contain spaces")
+    .matches(
+      passwordRegex,
+      "Password must be 8 characters or more and contain at least one special character"
+    ),
   confirmPassword: string()
     .required("Confirm password is required")
     .oneOf([ref("password"), null], "Passwords must match"),
