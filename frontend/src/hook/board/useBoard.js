d285cf7def859f5770ab9c256ad7008e971fec72
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-08-16 16:01:40 +0530

    all okay working

--- frontend/src/hook/board/useBoard.js
+++ frontend/src/hook/board/useBoard.js
@@ -11,7 +11,7 @@
 // import { isTaskDisplayed } from "../../redux/boolean/booleanSlice";
 // import { useGetProjectQuery } from "../../hook/useProjectQuery";
 // import { useNavigate } from "react-router-dom";
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo, useCallback } from "react";
 import { useGetTaskAccordingToStatus } from "../../hook/useTaskQuery";
 import { useGetColumnQuery } from "../useColumnQuery";
 import { useDispatch, useSelector } from "react-redux";
@@ -32,7 +32,7 @@ const useBoard = () => {
   const [isAddColBtnClicked, setIsAddColBtnClicked] = useState(false);
   const dispatch = useDispatch();
   const { is_updating_task } = useSelector(booleanDataInStore);
-  const [fff, setFff] = useState([]);
+  const [finalTaskUpdate, setFinalTaskUpdate] = useState([]);
   const { mutate: updateTaskWithIndex } = useUpdateTaskQuery();
   /**
    * when ever value get's true set it to false
@@ -64,13 +64,13 @@ const useBoard = () => {
    */
   let finalState = useMemo(() => {
     if (is_updating_task) {
-      return fff;
+      return finalTaskUpdate;
     }
     return columnDataWithTaskProperty?.map((column) => ({
       ...column,
       tasks: data.flat().filter((task) => task?.status === column.name),
     }));
-  }, [columnDataWithTaskProperty, data, is_updating_task, fff]);
+  }, [columnDataWithTaskProperty, data, is_updating_task, finalTaskUpdate]);
 
   /**
    * handle dispaly column button
@@ -82,204 +82,202 @@ const useBoard = () => {
   /**
    * handle drag end
    */
-  const handleDragEnd = (result) => {
-    const { source, destination } = result;
-    const allTasksWithColumns = finalState;
-    let finalStateOfTask = [];
-    if (!destination) return;
-
-    /**
-     * if task is moved into same column
-     */
-
-    if (destination.droppableId === source.droppableId) {
-      // find in which row task is drag and dropped
-      // Get current state and its setter for the status
-      const columnWhereTaskIsDragAndDrop = finalState.filter(
-        (item) => item.name === destination.droppableId
-      );
-
-      // find which task is moved
-      const movedTask = columnWhereTaskIsDragAndDrop[0]?.tasks[source.index];
-
-      // for update
-      const updateTaskForBE = {
-        ...movedTask,
-        currentIndex: destination.index,
-      };
-
-      // for local
-      const updateTaskForLocal = {
-        ...movedTask,
-        index: destination.index,
-      };
-
-      // now update the local state as well
+  const handleDragEnd = useCallback(
+    (result) => {
+      const { source, destination } = result;
+      const allTasksWithColumns = finalState;
+      let finalStateOfTask = [];
+      if (!destination) return;
 
       /**
-       * get all the task ( task is moving from top to bottom )
+       * if task is moved into same column
        */
 
-      if (destination.index > source.index) {
-        const allTheTaskThatHavelessIndexValueOfDestinationIndex =
-          columnWhereTaskIsDragAndDrop[0]?.tasks
+      if (destination.droppableId === source.droppableId) {
+        // find in which row task is drag and dropped
+        // Get current state and its setter for the status
+        const columnWhereTaskIsDragAndDrop = finalState.filter(
+          (item) => item.name === destination.droppableId
+        );
+
+        // find which task is moved
+        const movedTask = columnWhereTaskIsDragAndDrop[0]?.tasks[source.index];
+
+        // for update
+        const updateTaskForBE = {
+          ...movedTask,
+          currentIndex: destination.index,
+        };
+
+        // for local
+        const updateTaskForLocal = {
+          ...movedTask,
+          index: destination.index,
+        };
+
+        // now update the local state as well
+
+        /**
+         * get all the task ( task is moving from top to bottom )
+         */
+
+        if (destination.index > source.index) {
+          const allTheTaskThatHavelessIndexValueOfDestinationIndex =
+            columnWhereTaskIsDragAndDrop[0]?.tasks
+              .filter(
+                (item) =>
+                  item.index <= destination.index && item.index !== source.index
+              )
+              .map((item) => ({ ...item, index: item.index - 1 }));
+
+          // other remaining tasks
+          const remainingTask = columnWhereTaskIsDragAndDrop[0]?.tasks.filter(
+            (item) =>
+              item.index > destination.index && item.index !== source.index
+          );
+          finalStateOfTask = [
+            ...allTheTaskThatHavelessIndexValueOfDestinationIndex,
+            ...remainingTask,
+            updateTaskForLocal,
+          ].sort((a, b) => a.index - b.index);
+
+          /**
+           * update the task in array as well
+           */
+          const updatedValues = allTasksWithColumns.map((item) => {
+            if (item.name === destination.droppableId) {
+              return { ...item, tasks: finalStateOfTask };
+            }
+            return item; // Return unchanged item for other columns
+          });
+          setFinalTaskUpdate(updatedValues);
+        } else {
+          /**
+           * if task is moving from bottom to top
+           */
+
+          const allTaskToUpdate = columnWhereTaskIsDragAndDrop[0]?.tasks
             .filter(
               (item) =>
-                item.index <= destination.index && item.index !== source.index
+                item.index >= destination.index && item.index < source.index
             )
-            .map((item) => ({ ...item, index: item.index - 1 }));
+            .map((item) => ({ ...item, index: item.index + 1 }));
 
-        // other remaining tasks
-        const remainingTask = columnWhereTaskIsDragAndDrop[0]?.tasks.filter(
-          (item) =>
-            item.index > destination.index && item.index !== source.index
-        );
-        finalStateOfTask = [
-          ...allTheTaskThatHavelessIndexValueOfDestinationIndex,
-          ...remainingTask,
-          updateTaskForLocal,
-        ].sort((a, b) => a.index - b.index);
+          // step - 2 find all task that have greater then index of destination
+          const remaningTaskHavingIndexGreaterThanDestination =
+            columnWhereTaskIsDragAndDrop[0]?.tasks.filter(
+              (item) => item.index < destination.index
+            );
 
-        /**
-         * update the task in array as well
-         */
-        const updatedValues = allTasksWithColumns.map((item) => {
-          if (item.name === destination.droppableId) {
-            return { ...item, tasks: finalStateOfTask };
-          }
-          return item; // Return unchanged item for other columns
-        });
-        console.log(updatedValues, "::::::allTaskWithColumns");
-        setFff(updatedValues);
+          // step - 3 find all task that have less then index of source
+          const remaningTaskHavingIndexLessThanDestination =
+            columnWhereTaskIsDragAndDrop[0]?.tasks.filter(
+              (item) => item.index > source.index
+            );
+
+          finalStateOfTask = [
+            ...remaningTaskHavingIndexGreaterThanDestination,
+            ...remaningTaskHavingIndexLessThanDestination,
+            ...allTaskToUpdate,
+            updateTaskForLocal,
+          ].sort((a, b) => a.index - b.index);
+
+          const updatedValues = allTasksWithColumns.map((item) => {
+            if (item.name === destination.droppableId) {
+              return { ...item, tasks: finalStateOfTask };
+            }
+            return item; // Return unchanged item for other columns
+          });
+          setFinalTaskUpdate(updatedValues);
+        }
+        // setShowRerender(true);
+        dispatch(isUpdatingTask(true));
+        updateTaskWithIndex(updateTaskForBE);
       } else {
         /**
-         * if task is moving from bottom to top
+         * Get the state where task is moved
          */
+        const columnWhereTaskIsDragAndDrop = finalState.filter(
+          (item) => item.name === destination.droppableId
+        );
 
-        const allTaskToUpdate = columnWhereTaskIsDragAndDrop[0]?.tasks
-          .filter(
-            (item) =>
-              item.index >= destination.index && item.index < source.index
-          )
-          .map((item) => ({ ...item, index: item.index + 1 }));
+        /**
+         * source tasks
+         */
+        const sourceOfTask = finalState.filter(
+          (item) => item.name === source.droppableId
+        );
 
-        // step - 2 find all task that have greater then index of destination
-        const remaningTaskHavingIndexGreaterThanDestination =
-          columnWhereTaskIsDragAndDrop[0]?.tasks.filter(
-            (item) => item.index < destination.index
-          );
+        // find which task is moved
+        const movedTask = sourceOfTask[0]?.tasks[source.index];
 
-        // step - 3 find all task that have less then index of source
-        const remaningTaskHavingIndexLessThanDestination =
-          columnWhereTaskIsDragAndDrop[0]?.tasks.filter(
-            (item) => item.index > source.index
-          );
+        // for payload
+        const updateTask = {
+          ...movedTask,
+          status: destination.droppableId,
+          currentIndex: destination.index,
+          prevStatus: source.droppableId,
+          prevIndex: source.index,
+        };
 
-        finalStateOfTask = [
-          ...remaningTaskHavingIndexGreaterThanDestination,
-          ...remaningTaskHavingIndexLessThanDestination,
-          ...allTaskToUpdate,
-          updateTaskForLocal,
+        // for local
+        const updateTaskInDestination = {
+          ...movedTask,
+          index: destination.index,
+          status: destination.droppableId,
+        };
+
+        // remove the task from Active state and update the index
+
+        const updatedIndexOfTaskInRangeOfSource = sourceOfTask[0]?.tasks
+          ?.map((item) => {
+            if (item.index === source.index) {
+              // Skip the item with the same index as the source index
+              return null; // Or return any unique value that won't be present in the actual data
+            } else if (item.index > source.index) {
+              return { ...item, index: item.index - 1 };
+            } else {
+              return item;
+            }
+          })
+          .filter((item) => item !== null) // Filter out the skipped items directly
+          .sort((a, b) => a.index - b.index);
+
+        // add the task into destination state and update the index
+        const updatedIndexOfTaskInRangeOfDestination =
+          columnWhereTaskIsDragAndDrop[0]?.tasks?.map((item) => {
+            if (item.index >= source.index) {
+              return { ...item, index: item.index + 1 };
+            } else {
+              return item;
+            }
+          });
+
+        const finalArrayWithDroppedTask = [
+          ...updatedIndexOfTaskInRangeOfDestination,
+          updateTaskInDestination,
         ].sort((a, b) => a.index - b.index);
+
+        /**
+         * final update
+         */
 
         const updatedValues = allTasksWithColumns.map((item) => {
           if (item.name === destination.droppableId) {
-            return { ...item, tasks: finalStateOfTask };
+            return { ...item, tasks: finalArrayWithDroppedTask };
+          }
+          if (item.name === source.droppableId) {
+            return { ...item, tasks: updatedIndexOfTaskInRangeOfSource };
           }
           return item; // Return unchanged item for other columns
         });
-        setFff(updatedValues);
+        setFinalTaskUpdate(updatedValues);
+        updateTaskWithStatus(updateTask);
+        dispatch(isUpdatingTask(true));
       }
-      // setShowRerender(true);
-      dispatch(isUpdatingTask(true));
-      updateTaskWithIndex(updateTaskForBE);
-    } else {
-      // find in which row task is drag and dropped
-      // Get current state and its setter for the status
-      const columnWhereTaskIsDragAndDrop = finalState.filter(
-        (item) => item.name === destination.droppableId
-      );
-
-      /**
-       * source tasks
-       */
-      const sourceOfTask = finalState.filter(
-        (item) => item.name === source.droppableId
-      );
-
-      // find which task is moved
-      const movedTask = sourceOfTask[0]?.tasks[source.index];
-
-      // for payload
-      const updateTask = {
-        ...movedTask,
-        status: destination.droppableId,
-        currentIndex: destination.index,
-        prevStatus: source.droppableId,
-        prevIndex: source.index,
-      };
-      updateTaskWithStatus(updateTask);
-
-      // for local
-      const updateTaskInDestination = {
-        ...movedTask,
-        index: destination.index,
-        status: destination.droppableId,
-      };
-
-      // remove the task from Active state and update the index
-
-      const updatedIndexOfTaskInRangeOfSource = sourceOfTask[0]?.tasks
-        ?.map((item) => {
-          if (item.index === source.index) {
-            // Skip the item with the same index as the source index
-            return null; // Or return any unique value that won't be present in the actual data
-          } else if (item.index > source.index) {
-            return { ...item, index: item.index - 1 };
-          } else {
-            return item;
-          }
-        })
-        .filter((item) => item !== null) // Filter out the skipped items directly
-        .sort((a, b) => a.index - b.index);
-
-      // add the task into destination state and update the index
-      const updatedIndexOfTaskInRangeOfDestination =
-        columnWhereTaskIsDragAndDrop[0]?.tasks?.map((item) => {
-          if (item.index >= source.index) {
-            return { ...item, index: item.index + 1 };
-          } else {
-            return item;
-          }
-        });
-
-      const finalArrayWithDroppedTask = [
-        ...updatedIndexOfTaskInRangeOfDestination,
-        updateTaskInDestination,
-      ].sort((a, b) => a.index - b.index);
-
-      console.log(
-        finalArrayWithDroppedTask,
-        ":::final array with dropped task"
-      );
-      /**
-       * final update
-       */
-
-      const updatedValues = allTasksWithColumns.map((item) => {
-        if (item.name === destination.droppableId) {
-          return { ...item, tasks: finalArrayWithDroppedTask };
-        }
-        if (item.name === source.droppableId) {
-          return { ...item, tasks: updatedIndexOfTaskInRangeOfSource };
-        }
-        return item; // Return unchanged item for other columns
-      });
-      setFff(updatedValues);
-      dispatch(isUpdatingTask(true));
-      console.log(updatedValues, "::::updatedValues:::");
-    }
-  };
+    },
+    [finalState]
+  );
 
   // console.log(finalState, ":::::::::column Data::::::::::");
   return {
