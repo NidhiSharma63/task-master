2df2f55c0c9d21af634cffc259638b174b9da6f7
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-08-24 11:28:47 +0530

    some basic checks aded

--- backend/utils/setSchedule.js
+++ backend/utils/setSchedule.js
@@ -8,7 +8,7 @@ const sendEmail = require("./sendEmail");
 async function fetchTasks() {
   const currentDate = new Date();
   const tasks = await Task.find({
-    dueDate: { $exists: true, $gte: currentDate },
+    dueDate: { $exists: true },
   });
 
   return tasks;
@@ -16,26 +16,29 @@ async function fetchTasks() {
 
 async function rescheduleReminders() {
   const tasks = await fetchTasks();
+  console.log(tasks);
   // Clear existing scheduled jobs
   schedule.cancelJob();
 
   // // Schedule jobs for the updated due dates
   tasks.forEach(async (task) => {
-    // get user from db which task is running now
-    const user = await User.findOne({ _id: task.userId });
+    if (task.dueDate) {
+      // get user from db which task is running now
+      const user = await User.findOne({ _id: task.userId });
 
-    // luxon required ISO String to convert it to UTC
-    const date = new Date(task.dueDate).toISOString();
-    const utcDate = DateTime.fromISO(date, { zone: "utc" });
+      // luxon required ISO String to convert it to UTC
+      const date = new Date(task.dueDate).toISOString();
+      const utcDate = DateTime.fromISO(date, { zone: "utc" });
 
-    // now changing UTC to user time Zone
-    const userTimeZoneConvertedDate = utcDate.setZone(user?.timeZone);
+      // now changing UTC to user time Zone
+      const userTimeZoneConvertedDate = utcDate.setZone(user?.timeZone);
 
-    // now using toISO() to convert object to string
-    schedule.scheduleJob(userTimeZoneConvertedDate.toISO(), function () {
-      // sendReminderEmail(task.title);
-      sendEmail(user, task);
-    });
+      // now using toISO() to convert object to string
+      schedule.scheduleJob(userTimeZoneConvertedDate.toISO(), function () {
+        // sendReminderEmail(task.title);
+        sendEmail(user, task);
+      });
+    }
   });
 }
 
