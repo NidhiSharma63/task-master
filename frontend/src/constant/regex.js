bdbd5f0f3de8676a482d20f7be1c0b9479faacdd
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-09-20 14:25:16 +0530

    period allowed in password

--- frontend/src/constant/regex.js
+++ frontend/src/constant/regex.js
@@ -1,2 +1,2 @@
 export const regexToTargetAllBlankSpaces = /^\s*\S[^]*$/;
-export const passwordRegex = /^(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+export const passwordRegex = /^(?=.*[!@#$%^&*.])[A-Za-z\d!@#$%^&*.]{8,}$/;
