1b0853d108dffc3686b9e5099e4ef6dbeda399bd
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-09-25 13:29:41 +0530

    full fledge Markdown added

--- frontend/src/components/userPages/components/PageComponent.jsx
+++ frontend/src/components/userPages/components/PageComponent.jsx
@@ -1,88 +1,77 @@
-import { Box, Button, Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
-import PagesModal from "src/components/userPages/components/PagesModal";
-import usePage from "src/hook/page/usePage";
-import ContentEditable from "react-contenteditable";
-import sanitizeHtml from "sanitize-html";
-import { ACTIONS_FOR_EDITABLE_CONTENT } from "src/constant/Misc";
-import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import { Box, Button } from '@mui/material';
+import { Editor } from '@tinymce/tinymce-react';
+import PagesModal from 'src/components/userPages/components/PagesModal';
+
+import usePage from 'src/hook/page/usePage';
 
 const PageComponent = () => {
-  const { handleClick, handleChange, openAccordianOnClick, handleListCreation, innerHTML, editorRef, isAccordianOpen } =
-    usePage();
-
+  const { handleClick, editorRef, innerHTML } = usePage();
   return (
     <Box
       sx={{
-        display: "flex",
-        minWidth: "300px",
-        maxWidth: "80%",
-        width: "800px",
-        margin: "auto",
-        marginTop: "1rem",
-        height: "100%",
-      }}>
+        display: 'flex',
+        width: '98%',
+        marginLeft: '1rem',
+        height: 'calc(100vh - 70px)',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+      }}
+    >
       <Button
         variant="contained"
-        sx={{ position: "fixed", right: "1rem", color: "white", fontSize: ".7rem" }}
-        onClick={handleClick}>
+        sx={{
+          position: 'fixed',
+          right: '1rem',
+          top: '5rem',
+          color: 'white',
+          fontSize: '.7rem',
+          zIndex: 99,
+        }}
+        onClick={handleClick}
+      >
         Save
       </Button>
-
-      <ContentEditable
-        html={sanitizeHtml(innerHTML)}
-        className="editable-content"
-        ref={editorRef}
-        onChange={handleChange}
+      <Editor
+        apiKey="your-api-key"
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue={innerHTML}
+        init={{
+          content_css: 'dark',
+          height: '100%',
+          width: '100%',
+          menubar: false,
+          plugins: [
+            'advlist',
+            'autolink',
+            'lists',
+            'link',
+            'image',
+            'charmap',
+            'preview',
+            'anchor',
+            'searchreplace',
+            'visualblocks',
+            'code',
+            'fullscreen',
+            'insertdatetime',
+            'media',
+            'table',
+            'code',
+            'help',
+            'wordcount',
+          ],
+          toolbar:
+            'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style: `body { font-family:Helvetica,Arial,sans-serif; font-size:14px; background-color: rgb(18,18,18);color:white}
+           .tox-toolbar { background-color: black; }
+           .tox:not(.tox-tinymce-inline) .tox-editor-header{
+            background-color: black;
+           }`,
+        }}
       />
-      <Accordion
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          maxWidth: `${isAccordianOpen ? "45rem" : "3.3rem"}`,
-          borderRadius: `${isAccordianOpen ? ".3rem" : "50%"}`,
-          position: "fixed",
-          bottom: 0,
-          // left: "50%",
-          border: "none",
-          marginBottom: 2,
-          "&::before": {
-            backgroundColor: "transparent",
-          },
-        }}>
-        <AccordionSummary onClick={openAccordianOnClick}>
-          <AddOutlinedIcon sx={{ cursor: "pointer" }} />
-        </AccordionSummary>
-        <AccordionDetails
-          sx={{
-            display: "flex",
-            flexDirection: "row", // Display children horizontally
-            justifyContent: "flex-start", // Align children to the start
-            gap: "8px",
-            alignItems: "center",
-            mt: 1.5,
-          }}>
-          {ACTIONS_FOR_EDITABLE_CONTENT.map((item) => {
-            return (
-              <Button
-                key={item.value}
-                arg={item.value}
-                name={item.value === "h1" || item === "h2" || item === "h3" ? "heading" : ""}
-                onMouseDown={(evt) => {
-                  evt.preventDefault(); // Avoids loosing focus from the editable area
-                  if (item.value === "ul") {
-                    handleListCreation("ul");
-                  } else if (item.value === "ol") {
-                    handleListCreation("ol");
-                  } else {
-                    document.execCommand(item.key, false, item.value); // Send the command to the browser
-                  }
-                }}>
-                {item.value}
-              </Button>
-            );
-          })}
-        </AccordionDetails>
-      </Accordion>
       <PagesModal />
     </Box>
   );
