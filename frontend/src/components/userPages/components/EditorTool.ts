6d922da94424cb4d32d2fd6f6e84207adf8df2c7
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-10-16 08:08:19 +0530

    convertToBlob new file created

--- frontend/src/components/userPages/components/EditorTool.ts
+++ frontend/src/components/userPages/components/EditorTool.ts
@@ -24,8 +24,8 @@ import Table from '@editorjs/table';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import CustomImage from 'src/components/userPages/components/CustomImageTool';
 import { storage } from 'src/firebase/config';
+import convertToBlob from 'src/utils/convertToBlob';
 import { v4 } from 'uuid';
-
 /**
  * inteface
  */
@@ -45,27 +45,6 @@ interface IFile {
   type: string;
   webkitRelativePath: string;
 }
-
-const convertToBlob = (file: IFile): Promise<Blob> => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-
-    // Call readAsArrayBuffer to start the file reading process
-    // reader.readAsArrayBuffer(file as unknown as Blob);
-
-    reader.onloadend = () => {
-      const blob = new Blob([reader.result as ArrayBuffer], {
-        type: file.type,
-      });
-      resolve(blob);
-    };
-    reader.onerror = (error) => {
-      console.log('error occurred', error);
-      reject(error);
-    };
-    reader.readAsArrayBuffer(file as unknown as Blob);
-  });
-};
 
 export const tools = {
   CheckList: CheckList,
