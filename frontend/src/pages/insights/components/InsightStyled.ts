aeb7a71e27e1cce9f98aed6d64ac0021dc2c967e
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-10-18 09:11:11 +0530

    InsighStyled.js converted into tsx

---
+++ frontend/src/pages/insights/components/InsightStyled.ts
@@ -0,0 +1,7 @@
+import { Select } from '@mui/material';
+import { styled } from '@mui/material/styles';
+export const MuiSelect = styled(Select)(() => ({
+  fontSize: '0.905rem',
+  border: 'none',
+  '.MuiOutlinedInput-notchedOutline': { border: 0 },
+}));
